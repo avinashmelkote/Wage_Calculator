7a90999ab358d40fdf60599dc271a84c5796ac7c
avinashmelkote <avinashmelkote@gmail.com>
2017-01-24 20:04:46 -0500

    Add files via upload

---
+++ wagecalculator.py
@@ -0,0 +1,20 @@
+hours  = raw_input('Enter the number of hours you have worked this week: ')
+rate = raw_input('Enter your hourly wage rate: ')
+hours = float(hours)
+rate = float(rate)
+if hours > 40:
+	try: 
+		weeklypay = (40 * rate) + ((hours-40) * 1.5 * rate)
+		print weeklypay
+		overtime = (hours-40) * 1.5 * float(rate)
+		print 'Your total pay for the week is:','$',weeklypay
+		print 'This includes','$',overtime,'for',(hours-40), 'hours worked overtime.'
+	except: 
+		print 'Please enter a numerical value for both options.'
+elif hours <= 40:
+	try: 
+		weeklypay = hours * float(rate)
+		print 'Your total pay for the week is:','$',weeklypay
+		print 'You are not eligibile for overtime pay because you worked less than 40 hours this week.'
+	except: 
+		print 'Please enter a numerical value for both options.'
