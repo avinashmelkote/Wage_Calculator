891ae59ac7e59e2596b715acf2e91c4acfd9c82e
avinashmelkote <avinashmelkote@gmail.com>
2017-01-30 20:11:56 -0500

    Update wagecalculator.py

--- wagecalculator.py
+++ wagecalculator.py
@@ -1,18 +1,28 @@
-#Calculate weekly pre-tax earnings including 1.5*wage for each hour above 40.
-hours = raw_input('Enter the number of hours you worked this week: ')
-rate = raw_input('Enter your hourly rate of pay: ')
-if hours <= 40:
-	try:
-		pay = float(hours) * float(rate)
-		print 'Your total pre-tax earnings this week are:','$',pay
-	except:
-		print 'Please enter a numeric input in both fields.'
-		quit()
-elif hours > 40:
-	try:
-		pay = 40*float(rate) + (float(hours)-40)*1.5*float(rate)
-		print 'Your total pre-tax earnings this week are:','$',pay
-		print 'You are eligible for overtime pay of','$',(float(hours)-40)*1.5*float(rate),'for',(float(hours)-40), 'hours in excess of 40 hours this week.'
-	except:
-		print 'Please enter a numeric input in both fields.'
-		quit()
+# Compute weekly pre-tax earnings including potential overtime pay.
+# Catch exceptions with try and except
+
+def computepay(hours, rate):
+	if hours > 40:
+		pay = (40 * rate) + (hours-40)*rate*1.5
+		return pay
+	elif hours <= 40:
+		pay = hours * rate
+		return pay
+
+try:
+	hours = raw_input('Enter Hours: ')
+	hours = float(hours)
+	rate = raw_input('Enter Rate: ')
+	rate = float(rate)
+except:
+	print 'Error, please enter numeric input'
+
+pay = computepay(hours, rate)
+
+print 'Your weekly pre-tax earnings are:', pay
+
+if hours > 40:
+	print 'You are eligible for overtime pay of','$',(hours-40)*rate*1.5,'for',(hours-40),'hours above 40 this week.'
+else:
+	print 'You are not eligible for overtime pay because you did not work over 40 hours this week.'
+
